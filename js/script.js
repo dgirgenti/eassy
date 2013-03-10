c78e822d1d392bb764f56ff48110ca059b31c340
Dan Girgenti <dangirgenti@gmail.com>
2013-03-09 23:44:27 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+var Eassy = Eassy || {};
+
+Eassy.Index = $(document).ready(function() {
+	console.log(Eassy.parseCSS(Eassy.getCSSJSON()))
+	console.log(Eassy.parseCSS(Eassy.getCSSJSON(),true))
+});
+
+
+// Takes in a set of CSS JSON objects and an 
+// optional boolean to denote SASS output 
+// which defaults to false
+//
+// parseCSS(cssJSON[,sass])
+//
+Eassy.parseCSS = function (cssJSON, sass) {
+		sass = sass || false
+		css = ""
+		for (i in cssJSON) {
+			css += cssJSON[i].selector
+			if (!sass) css += " {"
+			css += "\n"
+			for (j in cssJSON[i].properties) {
+				p = cssJSON[i].properties[j]
+				css += "\t" + p.prop + ": " + p.attr + ";\n"
+			}
+			if (!sass) css += "}"
+			css += "\n"
+		}
+		return css
+}
+
+Eassy.getCSSJSON = function () {
+		return [
+		{
+			"selector" : "body",
+			"properties" : [
+				{ 
+					"prop" : "font-family", 
+					"attr" : "Arial, sans-serif"
+				},
+				{
+					"prop" : "color",
+					"attr" : "#333333"
+				}
+			]
+		},
+		{
+			"selector" : "div.box",
+			"properties" : [
+				{ 
+					"prop" : "background-color", 
+					"attr" : "#ae9ea2"
+				},
+				{
+					"prop" : "border-radius",
+					"attr" : "3px"
+				}
+			]
+		}
+	]
+}
