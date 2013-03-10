82b247099a35b8df489845b5cb8f54f69aeaea50
Dan Girgenti <dangirgenti@gmail.com>
2013-03-10 14:38:04 -0400

    live textarea functional. implemented simple Eassy.getCSSJSON for now

--- js/script.js
+++ js/script.js
@@ -3,8 +3,12 @@ var Eassy = Eassy || {};
 Eassy.Index = $(document).ready(function() {
 	$('.colorpicker').colorpicker({format:"rgba"})
 		.on('changeColor', function(ev){
-	  	$(this).val(Eassy.RGBObjToString(ev.color.toRGB()))
-		});
+		  $(this).val(Eassy.RGBObjToString(ev.color.toRGB()))
+		  $(this).trigger('change')
+		})
+	$('form').change(function() {
+		$('.live textarea').val(Eassy.parseCSS(Eassy.getCSSJSON()))
+	})
 	//console.log(Eassy.parseCSS(Eassy.getCSSJSON()))
 	//console.log(Eassy.parseCSS(Eassy.getCSSJSON(),true))
 });
@@ -13,11 +17,11 @@ Eassy.Index = $(document).ready(function() {
 // as a string in rgba(r,g,b,a) format
 // 
 Eassy.RGBObjToString = function (rgba) {
-  	return "rgba(" 
-  		+ rgba.r + ","
-  		+ rgba.g + ","
-  		+ rgba.b + ","
-  		+ rgba.a + ")"
+	return "rgba(" 
+		+ rgba.r + ","
+		+ rgba.g + ","
+		+ rgba.b + ","
+		+ rgba.a + ")"
 }
 
 // Takes in a set of CSS JSON objects and an 
@@ -27,51 +31,34 @@ Eassy.RGBObjToString = function (rgba) {
 // parseCSS(cssJSON[,sass])
 //
 Eassy.parseCSS = function (cssJSON, sass) {
-		sass = sass || false
-		css = ""
-		for (i in cssJSON) {
-			css += cssJSON[i].selector
-			if (!sass) css += " {"
-			css += "\n"
-			for (j in cssJSON[i].properties) {
-				p = cssJSON[i].properties[j]
-				css += "\t" + p.prop + ": " + p.attr + ";\n"
-			}
-			if (!sass) css += "}"
-			css += "\n"
+	sass = sass || false
+	css = ""
+	for (i in cssJSON) {
+		css += cssJSON[i].selector
+		if (!sass) css += " {"
+		css += "\n"
+		for (j in cssJSON[i].properties) {
+			p = cssJSON[i].properties[j]
+			css += "\t" + p.prop + ": " + p.attr + ";\n"
 		}
-		return css
+		if (!sass) css += "}"
+		css += "\n"
+	}
+	return css
 }
 
 Eassy.getCSSJSON = function (form) {
-		selector = $('#selector').val()
-		/* 
-		return [
-		{
-			"selector" : "body",
-			"properties" : [
-				{ 
-					"prop" : "font-family", 
-					"attr" : "Arial, sans-serif"
-				},
-				{
-					"prop" : "color",
-					"attr" : "#333333"
-				}
-			]
-		},
-		{
-			"selector" : "div.box",
-			"properties" : [
-				{ 
-					"prop" : "background-color", 
-					"attr" : "#ae9ea2"
-				},
-				{
-					"prop" : "border-radius",
-					"attr" : "3px"
-				}
-			]
-		}
-	] */
+	selector = $('#selector').val()
+	props = []
+	$('input[id!=selector]').each(function() {
+		append = $(this).siblings('.add-on:last-child').html() || ""
+		props.push({
+			"prop" : $(this).parents('.controls').siblings('label').attr('for'),
+			"attr" : $(this).val() + append
+		})
+	})
+	return [{
+		"selector" : selector,
+		"properties" : props
+	}]
 }
